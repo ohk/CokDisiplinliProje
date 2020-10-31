c6a83dc3b641c557991ce1aa0d478fb19055dc43
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-10-31 19:12:50 +0300

    Unnecessary Files Deleted.

--- client/port2.js
+++
@@ -1,8 +0,0 @@
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('COM1', { baudRate: 9600 })
-
-setInterval(function () {
-    const data = `${Math.floor(Math.random() * (500 - 0 + 1))}\n`
-    port.write(data)
-  }, 1000);
--- client/ports.js
+++
@@ -1,6 +0,0 @@
-const SerialPort = require('serialport')
-SerialPort.list().then(ports => {
-    ports.forEach(function(port) {
-        console.log(port.path)
-    })
-})
