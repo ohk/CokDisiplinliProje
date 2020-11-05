f88781488e7a4990bc3fab03fbe830cd66f6b944
Ömer Hamid Kamışlı <ohk@Omer-MacBook-Pro.local>
2020-11-05 12:22:57 +0300

    Max degree added

--- client/index.js
+++ client/index.js
@@ -5,39 +5,54 @@ const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+/*
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+const port = new SerialPort("COM1", { baudRate: 9600 });
 
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('COM1', { baudRate: 9600 })
+const parser = new Readline();
+port.pipe(parser);
 
-const parser = new Readline()
-port.pipe(parser)
-var lastValue = 0
-parser.on('data', line => {
-  lastValue = line
-  console.log("COM Port 1: Alınan Veri" , line)
-})
+parser.on("data", (line) => {
+  lastValue = line;
+  console.log("COM Port 1: Alınan Veri", line);
+});
+*/
 
-const checkValue = async (name) => {
-  socket.on("client", (msg) => {
-    if (msg === "checkValue") {
-      socket.emit("client", {
-        header: "value",
-        name: name,
-        id: socket.id,
-        timeStamp: new Date().toLocaleString(),
-        value: lastValue,
-      });
+var lastValue = 0;
+var maxDegree = 35;
+var name = "sera 101";
+
+const checkValue = async () => {
+  socket.on("client", (message) => {
+    switch (message.header) {
+      case "check":
+        socket.emit("client", {
+          header: "value",
+          name: name,
+          id: socket.id,
+          timeStamp: new Date().toLocaleString(),
+          value: Math.floor(Math.random() * (500 - 0 + 1)),
+          maxDegree: maxDegree,
+        });
+        break;
+      case "degree":
+        console.log(message);
+        maxDegree = message.value;
+        //port.write(`${message.value}`);
+        break;
+      default:
+        break;
     }
   });
 };
 
-readline.question("Sera ismini giriniz?\n", (name) => {
-  console.log(`Sera İsmi: ${name}! Server ile bağlantı kuruluyor ...`);
-  socket.emit("client", {
-    header: "setName",
-    value: name,
+readline.question("Sera ismini giriniz?\n>", (answer1) => {
+  name = answer1;
+  readline.question("Maximum sıcaklığı giriniz?\n>", (answer2) => {
+    maxDegree = answer2;
+    console.log(`Sera İsmi: ${answer1}!\nMaximum Sıcaklık:${answer2}.`);
+    checkValue();
+    readline.close();
   });
-  checkValue(name);
-  readline.close();
 });
